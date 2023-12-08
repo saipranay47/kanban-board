07b60df4c03f6c01693277c1b77cfc8cb1101d29
saipranay47 <saipranayadepu07@gmail.com>
2023-12-09 01:03:35 +0530

    (feat): add card component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import Card from './components/board/Card';
-import './styles/App.css';
+import Card from "./components/board/Card";
+import "./styles/App.css";
 function App() {
+  const task = {
+    id: "CAM-4",
+    title: "Implement Email Notification System",
+    tag: ["Feature Request"],
+    userId: "usr-1",
+    status: "In progress",
+    priority: 3,
+  };
   return (
     <div>
-      <Card />
+      <Card task={task} />
     </div>
   );
 }
--- src/components/board/Card.js
+++ src/components/board/Card.js
@@ -1,10 +1,50 @@
-import React from 'react'
-import '../../styles/Card.css'
+import React from "react";
+import "../../styles/Card.css";
+import { UrgentIcon, MediumIcon, HighIcon, LowIcon, DotsIcon } from "../Icons";
+function Card({ task }) {
+  let Icon;
 
-function Card() {
+  switch (task.priority) {
+    case 0:
+      Icon = <DotsIcon />;
+      break;
+    case 1:
+      Icon = <LowIcon />;
+      break;
+    case 2:
+      Icon = <MediumIcon />;
+      break;
+    case 3:
+      Icon = <HighIcon />;
+      break;
+    case 4:
+      Icon = <UrgentIcon />;
+      break;
+    default:
+      Icon = <DotsIcon />;
+      break;
+  }
   return (
-    <div>Card</div>
-  )
+    <div className="card">
+      <div className="card_top">
+        <p className="id">{task.id}</p>
+        <div className="user">
+          <img src="https://i.pravatar.cc/300" alt={task.userId} />
+          <div className="dot"></div>
+        </div>
+      </div>
+      <p className="title">{task.title}</p>
+      <div className="tags">
+        <button className="priority tag">{Icon}</button>
+        {task.tag.map((tag, index) => (
+          <button className="tag" key={index}>
+            {" "}
+            <span className="circle"></span> {tag}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
